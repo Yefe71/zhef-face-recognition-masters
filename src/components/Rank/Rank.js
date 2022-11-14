c3e1d49df6d21a4483d7e47ea533655e903607c6
Jeff Anciado <jeffjustine7171@yahoo.com>
2022-11-14 19:55:23 +0800

    Stable

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -6,10 +6,10 @@ const Rank = ({name, entries}) => {
     return (
         <div>
             <div className = 'white f3'>
-                {`${name}, your current entry count is: `}
+                {`${name}, your current detection count is: `}
             </div>
             <div className = 'white f1'>
-                {`${entries}!`}
+                {`${entries}`}
             </div>
             
         </div>
